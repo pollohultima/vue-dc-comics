faa3850c8e9ff868e1943b866df0248e84dfa9a0
pollohultima <matteo.polonio@gmail.com>
2021-11-11 18:19:01 +0100

    Bonus done!

---
+++ src/components/Banner.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="main_footer">
+    <div class="container">
+      <div class="row">
+        <div class="col5">
+          <div class="icon">
+            <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+            <p>DIGITAL COMICS</p>
+          </div>
+        </div>
+        <div class="col5">
+          <div class="icon">
+            <img src="../assets/img/buy-comics-merchandise.png" alt="" />
+            <p>DC MERCHANDISE</p>
+          </div>
+        </div>
+        <div class="col5">
+          <div class="icon">
+            <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+            <p>SUBSCRIPTION</p>
+          </div>
+        </div>
+        <div class="col5">
+          <div class="icon">
+            <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
+            <p>DIGITAL COMICS</p>
+          </div>
+        </div>
+        <div class="col5">
+          <div class="icon">
+            <img
+              class="visa"
+              src="../assets/img/buy-dc-power-visa.svg"
+              alt=""
+            />
+            <p>DC POWER VISA</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+.main_footer {
+  height: 150px;
+  background-color: $dc_lightblue_color;
+  z-index: 1;
+  position: relative;
+  .container {
+    height: 150px;
+    .icon {
+      display: flex;
+      align-items: center;
+
+      p {
+        padding-left: 1rem;
+        color: white;
+        font-size: 10px;
+      }
+      img {
+        height: 55px;
+      }
+    }
+    .row {
+      height: 150px;
+    }
+  }
+  .visa {
+    width: 55px;
+  }
+}
+</style>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -2,57 +2,21 @@
   <div id="site_main">
     <div class="main_content">
       <section class="container">
-        <div class="row">
+        <div class="row_top">
           <h2>--> Content Goes Here</h2>
         </div>
       </section>
     </div>
-
-    <div class="main_footer">
-      <div class="container">
-        <div class="row">
-          <div class="col5">
-            <div class="icon">
-              <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
-              <p>DIGITAL COMICS</p>
-            </div>
-          </div>
-          <div class="col5">
-            <div class="icon">
-              <img src="../assets/img/buy-comics-merchandise.png" alt="" />
-              <p>DC MERCHANDISE</p>
-            </div>
-          </div>
-          <div class="col5">
-            <div class="icon">
-              <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
-              <p>SUBSCRIPTION</p>
-            </div>
-          </div>
-          <div class="col5">
-            <div class="icon">
-              <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
-              <p>DC POWER VISA</p>
-            </div>
-          </div>
-          <div class="col5">
-            <div class="icon">
-              <img
-                class="visa"
-                src="../assets/img/buy-dc-power-visa.svg"
-                alt=""
-              />
-              <p>DIGITAL COMICS</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Banner />
   </div>
   <!-- /#site_main -->
 </template>
 
 <script>
+import Banner from "./Banner.vue";
+export default {
+  components: { Banner },
+};
 </script>
 
 <style lang="scss">
@@ -60,37 +24,13 @@
 #site_main {
   background-color: #1c1c1c;
   .container {
-    .row {
+    .row_top {
+      display: flex;
       align-items: center;
       height: 130px;
       h2 {
         color: white;
       }
-    }
-  }
-  .main_footer {
-    height: 150px;
-    background-color: $dc_lightblue_color;
-    z-index: 1;
-    position: relative;
-    .row {
-      height: 150px;
-      .icon {
-        display: flex;
-        align-items: center;
-
-        p {
-          padding-left: 1rem;
-          color: white;
-          font-size: 10px;
-        }
-        img {
-          height: 55px;
-        }
-      }
-    }
-    .visa {
-      width: 55px;
     }
   }
 }
