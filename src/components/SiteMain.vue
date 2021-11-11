26572b654d1f04a3f53374cdded04c2e8318c22a
pollohultima <matteo.polonio@gmail.com>
2021-11-11 16:14:55 +0100

    Main completed

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -10,7 +10,42 @@
 
     <div class="main_footer">
       <div class="container">
-        <div class="row"></div>
+        <div class="row">
+          <div class="col5">
+            <div class="icon">
+              <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+              <p>DIGITAL COMICS</p>
+            </div>
+          </div>
+          <div class="col5">
+            <div class="icon">
+              <img src="../assets/img/buy-comics-merchandise.png" alt="" />
+              <p>DC MERCHANDISE</p>
+            </div>
+          </div>
+          <div class="col5">
+            <div class="icon">
+              <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+              <p>SUBSCRIPTION</p>
+            </div>
+          </div>
+          <div class="col5">
+            <div class="icon">
+              <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
+              <p>DC POWER VISA</p>
+            </div>
+          </div>
+          <div class="col5">
+            <div class="icon">
+              <img
+                class="visa"
+                src="../assets/img/buy-dc-power-visa.svg"
+                alt=""
+              />
+              <p>DIGITAL COMICS</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -37,6 +72,25 @@
   .main_footer {
     height: 150px;
     background-color: $dc_lightblue_color;
+    .row {
+      height: 150px;
+      .icon {
+        display: flex;
+        align-items: center;
+
+        p {
+          padding-left: 1rem;
+          color: white;
+          font-size: 10px;
+        }
+        img {
+          height: 55px;
+        }
+      }
+    }
+    .visa {
+      width: 55px;
+    }
   }
 }
 </style>
