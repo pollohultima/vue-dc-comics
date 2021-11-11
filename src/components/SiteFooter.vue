e8c684324ae71064cb24783e535d30b70cb8ffde
pollohultima <matteo.polonio@gmail.com>
2021-11-11 17:04:56 +0100

    Footer lists completed

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -24,6 +24,36 @@
                 </ul>
               </div>
             </div>
+            <div class="col-4">
+              <div class="dc">
+                <h3>DC</h3>
+                <ul>
+                  <li><a href="">Terms Of Use</a></li>
+                  <li><a href="">Privacy policy (New)</a></li>
+                  <li><a href="">Ad Choices</a></li>
+                  <li><a href="">Advertising</a></li>
+                  <li><a href="">Jobs</a></li>
+                  <li><a href="">Subscriptions</a></li>
+                  <li><a href="">Talent Workshops</a></li>
+                  <li><a href="">CPSC Certificates</a></li>
+                  <li><a href="">Ratings</a></li>
+                  <li><a href="">Shop Help</a></li>
+                  <li><a href="">Contact Us</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="sites">
+                <h3>SITES</h3>
+                <ul>
+                  <li><a href="">DC</a></li>
+                  <li><a href="">MAD Magazines</a></li>
+                  <li><a href="">DC Kids</a></li>
+                  <li><a href="">DC Universe</a></li>
+                  <li><a href="">DC Power Visa</a></li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -47,16 +77,16 @@ export default {
     height: 375px;
     background-image: url("../assets/img/footer-bg.jpg");
     .sub_cont {
-      padding: 35px 0;
-      width: 375px;
+      padding: 30px 0;
+      width: 400px;
       color: white;
       h3 {
-        padding: 10px 5px 0 0;
+        padding: 15px 0 10px 0;
+        font-size: 16px;
       }
       ul {
         list-style: none;
         li {
-          padding: 2px 0;
           a {
             text-decoration: none;
             color: white;
