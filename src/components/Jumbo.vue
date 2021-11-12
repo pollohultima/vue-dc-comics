f816bcf9671f5917f2c59052ff221ec17312f1be
pollohultima <matteo.polonio@gmail.com>
2021-11-12 11:42:19 +0100

    done jumbotron and starting displaying cards

---
+++ src/components/Jumbo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="jumbo"></div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+
+<style lang="scss">
+.jumbo {
+  height: 400px;
+  background-image: url("../assets/img/jumbotron.jpg");
+}
+</style>
